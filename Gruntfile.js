23037bf3fc2424c8e56c001ac685a5b64024085e
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-10-23 21:19:57 +0300

    build: don't rev fonts.

--- Gruntfile.js
+++ Gruntfile.js
@@ -248,8 +248,8 @@ module.exports = function (grunt) {
           src: [
             '<%= yeoman.dist %>/public/{,*/}*.js',
             '<%= yeoman.dist %>/public/{,*/}*.css',
-            '<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-            '<%= yeoman.dist %>/public/assets/fonts/*'
+            '<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+            //'<%= yeoman.dist %>/public/assets/fonts/*'
           ]
         }
       }
